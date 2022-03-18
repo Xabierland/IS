7ab45b7c7589ba7356a42b70223e2fc4c88ee281
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2022-03-18 01:27:18 +0100

    .

---
+++ lab3/Pantalla.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Pantalla extends JFrame {
+
+    public static void main(String[] args) {
+        JFrame frame = new Pantalla();
+        frame.setSize(390, 280);
+        frame.setVisible(true);
+    }
+}
